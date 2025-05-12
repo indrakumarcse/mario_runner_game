18caf57eea1fa8cc7fbb2c9ccc77695fe0f2cbb0
Indra Kumar <146826755+indrakumarcse@users.noreply.github.com>
2025-05-12 23:46:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1847,7 +1847,7 @@ function animation() {
                 createExplosion(bullet.mesh.position.clone());
                 isShaking = true;
                 shakeTime = 0;
-                playerHealth = Math.max(playerHealth - 5, 0); // Lose 20% health
+                playerHealth = Math.max(playerHealth - 7, 0); // Lose 20% health
                 const bulletSound = new Audio('music/bullet.wav');
                 bulletSound.volume = 1.0;
                 bulletSound.play();
